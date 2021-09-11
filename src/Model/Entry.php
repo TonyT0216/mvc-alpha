d99ca863ed19dfa27797bed4df1daf17b5715fae
Tony Toscano <tonymichael16@gmail.com>
2021-09-11 13:58:20 -0400

    PDO implementation for getting model information

--- src/Model/Entry.php
+++ src/Model/Entry.php
@@ -2,7 +2,19 @@
 
 namespace App\Model;
 
-class Entry
-{
+use PDO;
+use PDOException;
 
+class Entry extends Model
+{
+    public static function getAll()
+    {
+        try {
+            $db = static::getDb();
+            $stmt = $db->query('SELECT id, title, content FROM entries ORDER BY created_at');
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            $e->getMessage();
+        }
+    }
 }
