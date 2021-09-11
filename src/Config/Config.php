2dd334b09b289ba7e84e1dd9a32e517c7ec6f99f
Tony Toscano <tonymichael16@gmail.com>
2021-09-11 13:45:27 -0400

    Stripped out my server info, and password

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -4,5 +4,19 @@ namespace App\Config;
 
 class Config
 {
+    // Dev Web Server Name
+    const DB_HOST = ' ';
+
+    // Dev Database Name
+    const DB_NAME = '';
+
+    // Dev Database User Name
+    const DB_USER = '';
+
+    // Dev Database Password
+    const DB_PASSWORD = '';
+
+    // Server mode - false is production, true is development (affects the error logs)
+    const SHOW_ERRORS = false;
 
 }
