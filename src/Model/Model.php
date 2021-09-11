7ceec663d73860b54d34d884f653bc6f8947eca0
Tony Toscano <tonymichael16@gmail.com>
2021-09-11 13:59:17 -0400

    Base Model implementation

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -2,7 +2,22 @@
 
 namespace App\Model;
 
-class Model
-{
+use App\Config\Config;
+use PDO;
 
+abstract class Model
+{
+    public static function getDB(): ?PDO
+    {
+        static $db = null;
+
+        if ($db === null) {
+            $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' .
+                Config::DB_NAME . ';charset=utf8';
+            $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
+
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return $db;
+    }
 }
